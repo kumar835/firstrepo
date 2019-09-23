43089a92009c6177026021cc8c2ecd18f108d470
KUMAR <KUMAR@HOME>
2019-09-23 15:44:01 +0530

    second commit to git

--- SalessTool2019/src/com/faith/test/Main.java
+++ SalessTool2019/src/com/faith/test/Main.java
@@ -4,12 +4,12 @@ package com.faith.test;
 	public class Main {
 	public static void main(String[] args){
 	SalesData objSalesData=new SalesData();
-	System.out.println("Hello");
+	System.out.println("Hello UST Guys ! GOT REPOSITORY");
 	displayGreetings();
 	objSalesData.display();
 	}
 	private static void displayGreetings(){
-	System.out.println("Welcome to sales report");
+	System.out.println("Welcome to ooty");
 	}
 	}
 
